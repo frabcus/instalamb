fc2b3ec9c58bcaccc0dff52caac204dfc53d2e70
Francis Irving <francis@flourish.org>
2024-02-21 11:42:29 +0000

    Remove suggested posts. Needs more work to stop scrolling emptiness.

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
 console.log('Instalamb: Loading');
 
-// Right sidebar of home page at desktop widths
-function hideHomeSuggestedForYou() {
-    var found = find('span', 'Suggested for you', '@dir="auto"')
-    if (!found) {
-        return;
-    }
-
-    const holder = found.parentElement.parentElement.parentElement
-    shiftElemenOutTheWay(holder);
-}
-
-// This doesn't work as DOM mutates with scrolling - maybe intercept API responses??
-function hideSuggestedPosts() {
-    var found = find('span','Suggested Posts');
-    if (!found) {
-        return;
-    }
-
-    // Not really happy about counting up the parents like this, feels like will be flaky.
-    // There isn't much else to go on in the DOM - could we match against styles?
-    let nextArticle = found.parentElement.parentElement.parentElement.parentElement.nextElementSibling;
-    while (nextArticle) {
-        console.log('nextArticle', nextArticle);
-        if (nextArticle.tagName == 'ARTICLE') {
-            nextArticle.style.border = '5px solid red';
-            nextArticle.style.display = 'none';
-        }
-        nextArticle = nextArticle.nextElementSibling;
-    }
-}
-
 // Row of circles with stories in at top of home page
 function hideHomeStoriesMenu() {
     var found = document.querySelector('[role="menu"]');
@@ -43,6 +12,56 @@ function hideHomeStoriesMenu() {
     console.log('Instalamb: Shifted "Stories menu" off screen');
 }
 
+// Right sidebar of home page at desktop widths
+function hideHomeSuggestedForYou() {
+    var found = find('span', 'Suggested for you', '@dir="auto"')
+    if (!found) {
+        return;
+    }
+
+    const holder = found.parentElement.parentElement.parentElement
+    shiftElemenOutTheWay(holder);
+    console.log('Instalamb: Shifted "Suggested for you" off screen');
+}
+
+// When scrolling in feed, it has a div "You're all caught up, You've seen all new posts from the past 7 days."
+// This removes all posts after that, stopping infinite scroll. The posts are all HTML <article> elements.
+function hideHomeSuggestedPosts() {
+    var suggestedPosts = find('span','Suggested Posts');
+    if (!suggestedPosts) {
+        return;
+    }
+    shiftElemenOutTheWay(suggestedPosts);
+
+    // "You're all caught up" section, at same level as the articles
+    let caughtUp = suggestedPosts.parentElement.parentElement.parentElement.parentElement;
+
+    // // Make large scrolling parent area smaller
+    // let scrollArea = caughtUp.parentElement;
+    // console.log("padding***", getComputedStyle(scrollArea).getPropertyValue('padding-bottom'));
+
+    // Make sure earlier articles in front
+    let prevArticle = caughtUp.previousElementSibling;
+    while (prevArticle) {
+        if (prevArticle.tagName == 'ARTICLE') {
+            prevArticle.style.zIndex = null;
+        }
+        prevArticle = prevArticle.previousElementSibling;
+    }
+
+    // Put later articles at the back
+    let nextArticle = caughtUp.nextElementSibling;
+    while (nextArticle) {
+        if (nextArticle.tagName == 'ARTICLE') {
+            nextArticle.style.zIndex = '-10000';
+        }
+        nextArticle = nextArticle.nextElementSibling;
+    }
+
+    console.log('Instalamb: Hid suggested posts and prevented infinite scroll');
+}
+
+// Main entrypoint, when DOM changes
 const observer = new MutationObserver(mutations => {
     const pageCategory = detectPageCategory();
     console.log('Instalamb: Page is', pageCategory);
@@ -51,6 +70,7 @@ const observer = new MutationObserver(mutations => {
     } else if (pageCategory == 'home') {
         hideHomeStoriesMenu();
         hideHomeSuggestedForYou();
+        hideHomeSuggestedPosts();
     }
 });
 
